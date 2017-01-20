24091106508c57de3b3a7093f0e0d728bf0171ef
Daniel Probst <daenuprobst@gmail.com>
2017-01-20 17:31:42 +0100

    Replaced socket.io with more down to the metal websocket library.

--- main.go
+++ main.go
@@ -3,13 +3,23 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/googollee/go-socket.io"
+	"github.com/gorilla/websocket"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 	"strings"
 )
+
+type RequestMessage struct {
+	Command string   `json:"cmd"`
+	Content []string `json:"msg"`
+}
+
+type InitResponseMessage struct {
+	Command string        `json:"cmd"`
+	Content Configuration `json:"msg"`
+}
 
 type ColorMap struct {
 	Id          string   `json:"id"`
@@ -59,7 +69,54 @@ type Configuration struct {
 
 var dataDir string
 var config Configuration
-var variantIndices map[string][][]int
+
+var variantIndices map[string][][]uint32
+var smilesIndices map[string][]uint32
+var smilesLengths map[string][]uint16
+var idIndices map[string][]uint32
+var idLengths map[string][]uint16
+
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
+func underdarkInit(data []string) InitResponseMessage {
+	return InitResponseMessage{
+		Command: "cmd",
+		Content: config,
+	}
+}
+
+func serveUnderdark(w http.ResponseWriter, r *http.Request) {
+	c, err := upgrader.Upgrade(w, r, nil)
+
+	if err != nil {
+		log.Print("Error during upgrade:", err)
+		return
+	}
+
+	defer c.Close()
+
+	for {
+		msg := RequestMessage{}
+		err := c.ReadJSON(&msg)
+
+		if err != nil {
+			log.Println("Error while reading message:", err)
+			break
+		}
+
+		if msg.Command == "init" {
+			err = c.WriteJSON(underdarkInit(msg.Content))
+		}
+		if err != nil {
+			log.Println("Error while responding:", err)
+			break
+		}
+	}
+}
 
 func main() {
 	if len(os.Args) < 2 {
@@ -73,20 +130,47 @@ func main() {
 	checkConfig()
 	loadIndices()
 
-	server, err := socketio.NewServer(nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	server.On("connection", func(so socketio.Socket) {
-		log.Println("on connection")
-	})
-
-	http.Handle("/socket.io/", server)
 	http.Handle("/", http.FileServer(http.Dir("./asset")))
+	http.HandleFunc("/underdark", serveUnderdark)
 
-	log.Println("Serving at localhost:8080...")
+	log.Println("Serving at localhost:8080 ...")
 	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func loadIndices() {
+	// Initialize the maps holding the indices
+	variantIndices := make(map[string][][]uint32)
+	smilesIndices := make(map[string][]uint32)
+	smilesLengths := make(map[string][]uint16)
+	idIndices := make(map[string][]uint32)
+	idLengths := make(map[string][]uint16)
+
+	loopConfig(func(database *Database, path string) {
+		// Loading smiles and id indices and lengths
+		smilesIndices[database.Id] = make([]uint32, 10000000)
+		smilesLengths[database.Id] = make([]uint16, 10000000)
+		idIndices[database.Id] = make([]uint32, 10000000)
+		idLengths[database.Id] = make([]uint16, 10000000)
+
+		for i := 0; i < 10000000; i++ {
+			smilesIndices[database.Id][i] = 100
+			smilesLengths[database.Id][i] = 100
+			idIndices[database.Id][i] = 100
+			idLengths[database.Id][i] = 100
+		}
+	}, func(fingerprint *Fingerprint, path string) {
+		// Loading indices for coordinates
+
+	}, func(variant *Variant, path string) {
+		// Loading the bin contents (indices pointing to the
+		// smiles and ids
+		variantIndices[variant.Id] = make([][]uint32, 2)
+		variantIndices[variant.Id][0] = make([]uint32, 10000000)
+		variantIndices[variant.Id][1] = make([]uint32, 10000000)
+
+	}, func(colorMap *ColorMap, path string) {
+		// Nothing to do here
+	}, false)
 }
 
 func loadConfig() Configuration {
@@ -165,7 +249,7 @@ func checkConfig() {
 		if exists, _ := exists(colorMap.MapFile); !exists {
 			nf = append(nf, colorMap.MapFile)
 		}
-	})
+	}, true)
 
 	if len(nf) > 0 {
 		fmt.Println("The following files were not found. Please add the files or remove the entries from the config.")
@@ -183,22 +267,42 @@ func checkConfig() {
 func loopConfig(databaseCallback func(*Database, string),
 	fingerprintCallback func(*Fingerprint, string),
 	variantCallback func(*Variant, string),
-	colorMapCallback func(*ColorMap, string)) {
+	colorMapCallback func(*ColorMap, string),
+	updatePath bool) {
 	for i, _ := range config.Databases {
 		database := &config.Databases[i]
-		databasePath := concatPath(dataDir, database.Directory)
-		databaseCallback(database, databasePath)
+		var databasePath string
+		if updatePath {
+			databasePath = concatPath(dataDir, database.Directory)
+			databaseCallback(database, databasePath)
+		} else {
+			databaseCallback(database, database.Directory)
+		}
 		for j, _ := range database.Fingerprints {
 			fingerprint := &database.Fingerprints[j]
-			fingerprintPath := concatPath(databasePath, fingerprint.Directory)
-			fingerprintCallback(fingerprint, fingerprintPath)
+			var fingerprintPath string
+			if updatePath {
+				fingerprintPath = concatPath(databasePath, fingerprint.Directory)
+				fingerprintCallback(fingerprint, fingerprintPath)
+			} else {
+				fingerprintCallback(fingerprint, fingerprint.Directory)
+			}
 			for k, _ := range fingerprint.Variants {
 				variant := &fingerprint.Variants[k]
-				variantPath := concatPath(fingerprintPath, variant.Directory)
-				variantCallback(variant, variantPath)
+				var variantPath string
+				if updatePath {
+					variantPath = concatPath(fingerprintPath, variant.Directory)
+					variantCallback(variant, variantPath)
+				} else {
+					variantCallback(variant, variant.Directory)
+				}
 				for l, _ := range variant.ColorMaps {
 					colorMap := &variant.ColorMaps[l]
-					colorMapCallback(colorMap, variantPath)
+					if updatePath {
+						colorMapCallback(colorMap, variantPath)
+					} else {
+						colorMapCallback(colorMap, variant.Directory)
+					}
 				}
 			}
 		}
@@ -217,10 +321,6 @@ func concatPath(a string, b string) string {
 	}
 
 	return a + b
-}
-
-func loadIndices() {
-
 }
 
 func exists(path string) (bool, error) {
